a7cecbdef015d7fa6f07ee7b310c83b716255b52
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2020-12-31 16:49:37 -0500

    New version with basic documentation and support for non-flite controllers

--- flite/Program.cs
+++ flite/Program.cs
@@ -9,25 +9,36 @@ var parser = new IniDataParser ();
 var data = parser.Parse (new StreamReader (args [0]));
 var client = new HttpClient ();
 var json_options = new JsonSerializerOptions {
-    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+	PropertyNamingPolicy = JsonNamingPolicy.CamelCase
 };
 
 Console.WriteLine (":beer: | dropdown = false");
 foreach (var section in data.Sections) {
-    var hostname = section.Name;
-    Console.WriteLine ("---");
-    try {
-        var props = section.Properties;
-        var streamTask = client.GetStreamAsync ($"http://{hostname}/getValuesBlack");
-        var values = await JsonSerializer.DeserializeAsync<FliteValues> (await streamTask, json_options);
-        Console.WriteLine ($"{props ["Name"]} @ {DateTime.Now} | color=white");
-        Console.WriteLine ($"   Volume: {values.Level} {values.LevelUnits} Left | trim=false");
-        Console.WriteLine ($"   Temperature: {values.Temperature} {values.TemperatureUnits} | trim=false");
-        Console.WriteLine ($"   Pressure: {values.Pressure} {values.PressureUnits} | trim=false");
-        Console.WriteLine ($"Configure '{hostname}'... | href=http://{hostname}/ trim=false");
-    }
-    catch {
-        Console.WriteLine ($"{hostname}' is offline");
-    }
+	var name = section.Name;
+	var get_url = String.Empty;
+	try {
+		var props = section.Properties;
+		get_url = props ["GetUrl"];
+		if (String.IsNullOrEmpty (get_url))
+			continue;
+		Console.WriteLine ("---");
+		var streamTask = client.GetStreamAsync (get_url);
+		var values = await JsonSerializer.DeserializeAsync<FliteValues> (await streamTask, json_options);
+		Console.WriteLine ($"{section.Name} @ {DateTime.Now} | color=white");
+		Console.WriteLine ($"   Volume: {values.Level} {values.LevelUnits} Left | trim=false");
+		Console.WriteLine ($"   Temperature: {values.Temperature} {values.TemperatureUnits} | trim=false");
+		Console.WriteLine ($"   Pressure: {values.Pressure} {values.PressureUnits} | trim=false");
+		var config_url = props ["ConfigUrl"];
+		if (!String.IsNullOrEmpty (get_url))
+			Console.WriteLine ($"Configure... | href={config_url} trim=false");
+	}
+	catch {
+		if (String.IsNullOrEmpty (get_url)) {
+			Console.WriteLine ($"No 'GetUrl=' found inside section {name} of the ini file");
+		} else {
+			var get_uri = new Uri (get_url);
+			Console.WriteLine ($"'{get_uri.Host}' is offline");
+		}
+	}
 }
 return 0;
--- flite/README.md
+++ flite/README.md
@@ -1,1 +1,33 @@
-[Bitbar](https://github.com/matryer/bitbar) plugin for [Flite controllers](https://www.flitesense.com/)
+# Flite Plugin
+
+[Bitbar](https://github.com/matryer/bitbar) plugin for [Flite](https://www.flitesense.com/) compatible controllers.
+
+## Requirements
+
+* macOS 10.13+
+* [Bitbar](https://github.com/matryer/bitbar)
+* [.NET 5.0](https://dotnet.microsoft.com/download/dotnet/5.0)
+
+## Build
+
+```bash
+$ dotnet publish
+Microsoft (R) Build Engine version 16.8.0+126527ff1 for .NET
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+  Determining projects to restore...
+  Restored /Users/poupou/git/spouliot/bitbar-plugins/flite/flite.csproj (in 194 ms).
+  flite -> /Users/poupou/git/spouliot/bitbar-plugins/flite/bin/Debug/net5.0/flite.dll
+  flite -> /Users/poupou/git/spouliot/bitbar-plugins/flite/bin/Debug/net5.0/publish/
+```
+
+## Setup
+
+Example script `~/bitbar/flite.10s.sh`
+
+```bash
+#!/bin/bash
+/usr/local/share/dotnet/dotnet ~/git/spouliot/bitbar-plugins/flite/bin/Debug/net5.0/publish/flite.dll ~/git/spouliot/bitbar-plugins/flite/flite.ini
+```
+
+The `.10s.` part of script name tells bitbar to execute it every 10 seconds.
